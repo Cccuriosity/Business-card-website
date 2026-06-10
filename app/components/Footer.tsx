c3cc8928ac39fb59c12372f77f2be2e3a12e3a31
Cccuriosity <levshin.iliya@yandex.ru>
2026-06-11 00:10:12 +1000

    Fix Links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <div className={styles.Footer}>
-            <a className={styles.Link} href="https://wa.me/79025223190">
+            <a className={styles.Link} href="https://wa.me/79025223190" target="_blank">
                 <Image
                     src="/Whatsapp.png"
                     alt="Ватсапп"
@@ -16,6 +16,7 @@ export default function Footer() {
             <a
                 className={styles.Link}
                 href="https://max.ru/u/f9LHodD0cOKmeDbUrFsPQhIzbS9plAyv5aCbRIsvm4Ata9x7KlVfC28ZFaY"
+                target="_blank"
             >
                 <Image src="/Max.png" alt="Макс" width={80} height={80} className={styles.Icon} />
             </a>

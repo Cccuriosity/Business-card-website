42b2f4bbfaccabe4069b6fd1b81f59c1d64c7455
Cccuriosity <levshin.iliya@yandex.ru>
2026-06-11 00:07:55 +1000

    Add links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Footer() {
     return (
         <div className={styles.Footer}>
-            <a className={styles.Link}>
+            <a className={styles.Link} href="https://wa.me/79025223190">
                 <Image
                     src="/Whatsapp.png"
                     alt="Ватсапп"
@@ -13,17 +13,11 @@ export default function Footer() {
                     className={styles.Icon}
                 />
             </a>
-            <a className={styles.Link}>
+            <a
+                className={styles.Link}
+                href="https://max.ru/u/f9LHodD0cOKmeDbUrFsPQhIzbS9plAyv5aCbRIsvm4Ata9x7KlVfC28ZFaY"
+            >
                 <Image src="/Max.png" alt="Макс" width={80} height={80} className={styles.Icon} />
-            </a>
-            <a className={styles.Link}>
-                <Image
-                    src="/Telegram.png"
-                    alt="Телеграмм"
-                    width={80}
-                    height={80}
-                    className={styles.Icon}
-                />
             </a>
         </div>
     );
